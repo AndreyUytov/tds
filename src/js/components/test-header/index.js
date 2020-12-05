b9d49e7117b291d2b0726965b70596de0a7779e5
Андрей <uytov1991@gmail.com>
2020-12-05 15:37:08 +0300

    закончил компоненты для страницы с чекбоксами

--- src/js/components/test-header/index.js
+++ src/js/components/test-header/index.js
@@ -7,23 +7,41 @@ customElements.define(
       this.attachShadow({ mode: 'open' })
       this.totalQuestions = +this.getAttribute('total')
       this.currentQuest = +this.getAttribute('current')
+      this.testStarted = new Date(
+        +this.getAttribute('test-started-ms') || Date.now()
+      )
+      this.timeLimit = +this.getAttribute('time-limit-ms') || 1200000
       this.ul = document.createElement('ul')
       this.ul.className = 'tabs-list'
       this.render()
       this.nav = this.shadowRoot.querySelector('nav')
+      this.timer = this.shadowRoot.querySelector('.test-header__timer-numbers')
+      this.timerRender()
       this.renderTabs()
       this.nav.append(this.ul)
+    }
+
+    timerRender() {
+      setInterval(() => {
+        let time = new Date(this.timeLimit - (Date.now() - this.testStarted))
+        let differenceMin = time.getMinutes()
+        if (differenceMin < 5) {
+          this.timer.style.color = 'var(--redThemeColor)'
+        }
+        let differenceSec = time.getSeconds()
+        this.timer.textContent = `${differenceMin} : ${differenceSec}`
+      }, 1000)
     }
 
     renderTabs() {
       for (let i = 1; i <= this.totalQuestions; i++) {
         let li = `
-        <li class = ${
+        <li ${
           i > this.currentQuest
-            ? 'tabs-list__item'
+            ? 'class = "tabs-list__item"'
             : i < this.currentQuest
-            ? 'tabs-list__item tabs-list__item--disabled'
-            : 'tabs-list__item tabs-list__item--active'
+            ? 'class = "tabs-list__item tabs-list__item--disabled"'
+            : 'class = "tabs-list__item tabs-list__item--active"'
         }> ${i}
         </li>`
 
@@ -42,7 +60,8 @@ customElements.define(
         <nav class="test-header__nav">
         </nav>
         <div class="test-header__timer">
-          <span class="test-header__timer-text">Время</span>18:58
+          <span class="test-header__timer-text">Время</span>
+          <span class="test-header__timer-numbers"></span>
         </div>
       </div>
     </header>
