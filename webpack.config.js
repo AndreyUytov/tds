ceb07214f3f5bed91240fde0c904257475fd8f73
Андрей <uytov1991@gmail.com>
2020-12-14 17:29:48 +0300

    сделать аватар

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,7 @@ function generateHtmlPlugins(templateDir) {
       filename: `${name}.html`,
       template: path.resolve(__dirname, `${templateDir}/${name}.${extension}`),
       inject: true,
+      minify: false,
     })
   })
 }
