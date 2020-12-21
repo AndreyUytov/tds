d92987518cc797b2b77992fda219deb386a8f818
Андрей <uytov1991@gmail.com>
2020-12-21 13:52:57 +0300

    refactor lk

--- src/js/utylity/help-popup.js
+++ src/js/utylity/help-popup.js
@@ -2,11 +2,11 @@ const snapHelp = document.querySelector('.snap-help')
 const popupHelp = document.querySelector('.help-popup__wrapper')
 
 if (snapHelp && popupHelp) {
-  let bodyWidth = document.body.getBoundingClientRect().width
   snapHelp.onclick = () => {
     popupHelp.style.display = 'flex'
     snapHelp.style.display = 'none'
-    document.body.style.width = bodyWidth + 'px'
+    document.body.style.width =
+      document.body.getBoundingClientRect().width + 'px'
     document.body.style.overflow = 'hidden'
   }
   const closeSnap = popupHelp.querySelector('.help-popup__close-snap')
