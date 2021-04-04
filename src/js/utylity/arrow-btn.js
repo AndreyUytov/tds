dc1f205cacaa519210a12d43325c892a3416ff1d
Андрей <uytov1991@gmail.com>
2021-04-04 17:02:39 +0300

    добавил сортировку по результату, увеличил размер блока Подразделение

--- src/js/utylity/arrow-btn.js
+++ src/js/utylity/arrow-btn.js
@@ -14,25 +14,14 @@ window.addEventListener('scroll', function() {
 let tooltip;
 
     document.onmouseover = function(event) {
-      // важно: быстро движущийся курсор может прыгнуть сразу к дочернему элементу, пропустив родительский
-      // так что событие mouseover произойдёт сразу на дочернем элементе.
-
       let anchorElem = event.target.closest('.list-section__item-column');
 
       if (!anchorElem) return;
 
-      // показываем подсказку и запоминаем её
       tooltip = showTooltip(anchorElem, anchorElem.textContent);
     }
 
     document.onmouseout = function() {
-      // возможно такое, что произошло событие mouseout, но мы всё ещё внутри элемента
-      // (оно было где-то внутри и всплыло)
-      // но в этом случае сразу же последует событие mouseover,
-      // то есть подсказка исчезнет и потом снова покажется
-      //
-      // к счастью, этого не будет видно,
-      // так как оба события происходят почти одновременно
       if (tooltip) {
         tooltip.remove();
         tooltip = false;
@@ -49,7 +38,6 @@ let tooltip;
 
       let coords = anchorElem.getBoundingClientRect();
 
-      // позиционируем подсказку над центром элемента
       let left = coords.left + (anchorElem.offsetWidth - tooltipElem.offsetWidth) / 2;
       if (left < 0) left = 0;
 
