969efee4654239d2fc0f10fb7cb23f70724677e8
Андрей <uytov1991@gmail.com>
2021-05-11 11:58:31 +0300

    добавил block back btn для блокирования кнопки возврата

--- src/js/utylity/history.js
+++
@@ -1,9 +0,0 @@
-let ruleForBlockBackButton = /order/
-let pathName = document.location.pathname
-if (ruleForBlockBackButton.test(pathName)) {
-  history.pushState(null, null, location.href);
-  window.onpopstate = function() {
-    history.go(1);
-  };
-}
-
